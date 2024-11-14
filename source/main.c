f4c4add61b4833b809d76648b2614c73f6414dd1
knight <karvin.jimenez@alternard.com>
2024-11-14 13:28:42 -0400

    cleaning

--- source/main.c
+++ source/main.c
@@ -4,13 +4,6 @@
 #include <gba_input.h>
 #include "graphics.h"
 
-#define MAIN_MENU 0
-#define SETTINGS_MENU 1
-
-#define MENU_MODE 0
-#define MATCH_MODE 1
-#define RESET_MODE 2
-
 #define PADDLE_HEIGHT 24
 #define PADDLE_WIDTH 8
 #define BALL_SIZE 8
@@ -23,6 +16,8 @@
 #define PLAYER_START_Y ((SCREEN_HEIGHT / 2) - (PADDLE_HEIGHT / 2))
 #define WINNING_SCORE 10
 
+bool paused = true;
+
 /* Show menu cursor on current selection */
 void setMenuCursor(int selection)
 {
@@ -33,56 +28,13 @@ void setMenuCursor(int selection)
     printChar(selector[1], MENU_TEXT_X - CHAR_PIX_SIZE, MENU_ITEM_1 + (selection)*LINE_HEIGHT);
 }
 
-/* Set Starting Values for New Game */
-void setupMatch(struct rect *humanPlayer, struct rect *computerPlayer,
-                struct rect *ball, int *humanScore, int *computerScore, int *pauseLength,
-                int *pauseCounter, bool *paused)
-{
-    humanPlayer->x = 1;
-    humanPlayer->y = PLAYER_START_Y;
-    humanPlayer->prevX = humanPlayer->x;
-    humanPlayer->prevY = humanPlayer->y;
-    humanPlayer->width = PADDLE_WIDTH;
-    humanPlayer->height = PADDLE_HEIGHT;
-    humanPlayer->velocityX = 0;
-    humanPlayer->velocityY = 0;
-    *humanScore = 0;
-
-    computerPlayer->x = SCREEN_WIDTH - PADDLE_WIDTH - 1;
-    computerPlayer->y = PLAYER_START_Y;
-    computerPlayer->prevX = computerPlayer->x;
-    computerPlayer->prevY = computerPlayer->y;
-    computerPlayer->width = PADDLE_WIDTH;
-    computerPlayer->height = PADDLE_HEIGHT;
-    computerPlayer->velocityX = 0;
-    computerPlayer->velocityY = 0;
-    *computerScore = 0;
-
-    ball->x = BALL_START_X;
-    ball->y = (SCREEN_HEIGHT / 2) - (BALL_SIZE / 2);
-    ball->prevX = ball->x;
-    ball->prevY = ball->y;
-    ball->width = BALL_SIZE;
-    ball->height = BALL_SIZE;
-    ball->velocityX = 2;
-    ball->velocityY = 2;
-
-    *pauseLength = NEW_GAME_PAUSE;
-    *pauseCounter = 0;
-    *paused = true;
-}
-
 /* Bounding Box Collision Detection */
-bool collision(
+bool hasCollision(
     int x1, int y1, int width1, int height1,
     int x2, int y2, int width2, int height2)
 {
-    if (x1 + width1 > x2 &&
-        x1 < x2 + width2 &&
-        y1 + height1 > y2 &&
-        y1 < y2 + height2)
-        return true;
-    return false;
+    return (x1 + width1 > x2 && x1 < x2 + width2 &&
+            y1 + height1 > y2 && y1 < y2 + height2);
 }
 
 /* Paddle Bounce Logic - Tweak as needed depending on paddle size */
@@ -100,16 +52,16 @@ void bounceOffPaddle(struct rect *playerPaddle, struct rect *ball)
 }
 
 /* Scoring Points */
-void playerScores(bool isHuman, struct rect *ball, int *humanScore, int *computerScore, int *pauseLength, bool *paused, int *gameMode)
+void playerScores(bool isHuman, struct rect *ball, int *humanScore, int *cpuScore, int *pauseLength)
 {
     /* Increment Score */
     int *playerScore;
     if (isHuman)
         playerScore = humanScore;
     else
-        playerScore = computerScore;
+        playerScore = cpuScore;
     *playerScore = *playerScore + 1;
-    *paused = true;
+    paused = true;
 
     /* If Winning Score, Show Winner and Reset */
     if (*playerScore >= WINNING_SCORE)
@@ -126,9 +78,6 @@ void playerScores(bool isHuman, struct rect *ball, int *humanScore, int *compute
             displayText(" CPU WINS ",
                         END_TEXT_X, END_TEXT_Y);
         }
-        *gameMode = RESET_MODE;
-
-        /* Else Keep Playing */
     }
     else
     {
@@ -138,88 +87,89 @@ void playerScores(bool isHuman, struct rect *ball, int *humanScore, int *compute
 }
 
 /* Game Logic */
-void matchMode(struct rect *humanPlayer, struct rect *computerPlayer,
-               struct rect *ball, int *humanScore, int *computerScore,
-               int *pauseLength, int *pauseCounter, bool *paused, int *gameMode)
+void matchMode(struct rect *player, struct rect *cpuPlayer,
+               struct rect *ball, int *playerScore, int *cpuScore,
+               int *pauseLength, int *pauseCounter)
 {
     /* If players are rallying */
-    if (!*paused)
+    if (!paused)
     {
         /* If ball has hit opponents wall, player scores */
         if (ball->x <= 3 && ball->velocityX < 0)
         {
-            ball->x = humanPlayer->x;
-            playerScores(false, ball, humanScore, computerScore,
-                         pauseLength, paused, gameMode);
+            ball->x = player->x;
+            playerScores(false, ball, playerScore, cpuScore, pauseLength);
         }
+
         else if (ball->x >= SCREEN_WIDTH - ball->width - 3 && ball->velocityX > 0)
         {
-            ball->x = computerPlayer->x + PADDLE_WIDTH - BALL_SIZE;
-            playerScores(true, ball, humanScore, computerScore, pauseLength, paused, gameMode);
+            ball->x = cpuPlayer->x + PADDLE_WIDTH - BALL_SIZE;
+            playerScores(true, ball, playerScore, cpuScore, pauseLength);
         }
         /* If ball hits ceiling or floor, bounce off */
         if (ball->y <= 0 && ball->velocityY < 0)
         {
             ball->velocityY *= -1;
         }
+
         else if (ball->y >= SCREEN_HEIGHT - ball->height && ball->velocityY > 0)
         {
             ball->velocityY *= -1;
         }
 
         /* Move human player based on input */
-        int keys_pressed, keys_released;
-        keys_pressed = keysDown();
-        keys_released = keysUp();
+        int keys_pressed = keysDown();
+        int keys_released = keysUp();
+
         if ((keys_released & KEY_UP) || (keys_released & KEY_DOWN))
         {
-            humanPlayer->velocityY = 0;
+            player->velocityY = 0;
         }
-        if ((keys_pressed & KEY_UP) && (humanPlayer->y >= 0))
+        if ((keys_pressed & KEY_UP) && (player->y >= 0))
         {
-            humanPlayer->velocityY = -2;
+            player->velocityY = -2;
         }
         if ((keys_pressed & KEY_DOWN) &&
-            (humanPlayer->y <= SCREEN_HEIGHT - humanPlayer->height))
+            (player->y <= SCREEN_HEIGHT - player->height))
         {
-            humanPlayer->velocityY = 2;
+            player->velocityY = 2;
         }
-        if ((humanPlayer->y <= 0 && humanPlayer->velocityY < 0) ||
-            ((humanPlayer->y >= SCREEN_HEIGHT - humanPlayer->height) &&
-             humanPlayer->velocityY > 0))
+        if ((player->y <= 0 && player->velocityY < 0) ||
+            ((player->y >= SCREEN_HEIGHT - player->height) &&
+             player->velocityY > 0))
         {
-            humanPlayer->velocityY = 0;
+            player->velocityY = 0;
         }
 
         /* Computer Player Movement Logic */
-        if (ball->y + (BALL_SIZE - 2) > computerPlayer->y + (PADDLE_HEIGHT / 2) &&
-            computerPlayer->y + PADDLE_HEIGHT <= SCREEN_HEIGHT &&
+        if (ball->y + (BALL_SIZE - 2) > cpuPlayer->y + (PADDLE_HEIGHT / 2) &&
+            cpuPlayer->y + PADDLE_HEIGHT <= SCREEN_HEIGHT &&
             ball->x > SCREEN_WIDTH / 4 &&
             !(ball->velocityY < -2) &&
             (ball->velocityX > 0 || ball->x > SCREEN_WIDTH / 2))
         {
-            computerPlayer->velocityY = 2;
+            cpuPlayer->velocityY = 2;
         }
-        else if (ball->y + (BALL_SIZE - 2) < computerPlayer->y + (PADDLE_HEIGHT / 2) &&
-                 computerPlayer->y >= 0 &&
+        else if (ball->y + (BALL_SIZE - 2) < cpuPlayer->y + (PADDLE_HEIGHT / 2) &&
+                 cpuPlayer->y >= 0 &&
                  ball->x > SCREEN_WIDTH / 4 &&
                  !(ball->velocityY > 2) &&
                  (ball->velocityX > 0 || ball->x > SCREEN_WIDTH / 2))
         {
-            computerPlayer->velocityY = -2;
+            cpuPlayer->velocityY = -2;
         }
         else
         {
-            computerPlayer->velocityY = 0;
+            cpuPlayer->velocityY = 0;
         }
 
         /* Update Positions */
-        if (!*paused)
+        if (!paused)
         {
             ball->x += ball->velocityX;
             ball->y += ball->velocityY;
-            humanPlayer->y += humanPlayer->velocityY;
-            computerPlayer->y += computerPlayer->velocityY;
+            player->y += player->velocityY;
+            cpuPlayer->y += cpuPlayer->velocityY;
         }
 
         /* Wait a moment after score before new rally */
@@ -230,68 +180,60 @@ void matchMode(struct rect *humanPlayer, struct rect *computerPlayer,
         if (*pauseCounter == (int)HALF_PAUSE)
         {
             ball->x = BALL_START_X;
-            humanPlayer->y = PLAYER_START_Y;
-            computerPlayer->y = PLAYER_START_Y;
+            player->y = PLAYER_START_Y;
+            cpuPlayer->y = PLAYER_START_Y;
         }
         if (*pauseCounter > *pauseLength)
         {
             *pauseCounter = 0;
-            *paused = false;
+            paused = false;
         }
     }
 
     /* Clear Previous Ball, Player Graphics */
     clearPrevious(ball);
-    clearPrevious(humanPlayer);
-    clearPrevious(computerPlayer);
+    clearPrevious(player);
+    clearPrevious(cpuPlayer);
 
-    /* Draw Static Elements */
-    if (*gameMode != RESET_MODE)
-        drawCenterLine();
+    drawCenterLine();
 
-    printHumanScore(score[*humanScore]);
-    printComputerScore(score[*computerScore]);
+    printHumanScore(score[*playerScore]);
+    printComputerScore(score[*cpuScore]);
     printPlayerSymbols();
 
     /* Draw Ball, Players at current positions */
     drawRect(ball, CLR_LIME);
-    drawRect(humanPlayer, CLR_BLUE);
-    drawRect(computerPlayer, CLR_BLUE);
+    drawRect(player, CLR_BLUE);
+    drawRect(cpuPlayer, CLR_BLUE);
 
     /* Update previous positions for clearing pixels */
     ball->prevX = ball->x;
     ball->prevY = ball->y;
-    humanPlayer->prevX = humanPlayer->x;
-    humanPlayer->prevY = humanPlayer->y;
-    computerPlayer->prevX = computerPlayer->x;
-    computerPlayer->prevY = computerPlayer->y;
+    player->prevX = player->x;
+    player->prevY = player->y;
+    cpuPlayer->prevX = cpuPlayer->x;
+    cpuPlayer->prevY = cpuPlayer->y;
 
     /* Check for paddle / ball collisions, if so ball bounces */
-    if (collision(
-            humanPlayer->x, humanPlayer->y,
-            humanPlayer->width, humanPlayer->height,
+    if (hasCollision(
+            player->x, player->y,
+            player->width, player->height,
             ball->x, ball->y,
             ball->width, ball->height) &&
         ball->velocityX < 0)
     {
-        bounceOffPaddle(humanPlayer, ball);
+        bounceOffPaddle(player, ball);
     }
-    if (collision(
-            computerPlayer->x, computerPlayer->y,
-            computerPlayer->width, computerPlayer->height,
+    if (hasCollision(
+            cpuPlayer->x, cpuPlayer->y,
+            cpuPlayer->width, cpuPlayer->height,
             ball->x, ball->y,
             ball->width, ball->height) &&
         ball->velocityX > 0)
     {
-        bounceOffPaddle(computerPlayer, ball);
+        bounceOffPaddle(cpuPlayer, ball);
     }
 
-    /* If Game has Ended, Clear Match Data */
-    if (*gameMode == RESET_MODE)
-    {
-        setupMatch(humanPlayer, computerPlayer, ball, humanScore,
-                   computerScore, pauseLength, pauseCounter, paused);
-    }
     return;
 }
 
@@ -303,17 +245,45 @@ int main(void)
     // Enable Vblank Interrupt, Allow VblankIntrWait
     irqEnable(IRQ_VBLANK);
 
+    /* Set screen to mode 3 */
+    SetMode(MODE_3 | BG2_ON);
+
     /* Match Variables */
     struct rect humanPlayer, computerPlayer, ball;
     int humanScore, computerScore;
     int pauseLength, pauseCounter;
-    bool paused;
-    setupMatch(&humanPlayer, &computerPlayer, &ball, &humanScore, &computerScore, &pauseLength, &pauseCounter, &paused);
 
-    int gameMode = MATCH_MODE;
+    humanPlayer.x = 1;
+    humanPlayer.y = PLAYER_START_Y;
+    humanPlayer.prevX = humanPlayer.x;
+    humanPlayer.prevY = humanPlayer.y;
+    humanPlayer.width = PADDLE_WIDTH;
+    humanPlayer.height = PADDLE_HEIGHT;
+    humanPlayer.velocityX = 0;
+    humanPlayer.velocityY = 0;
+    humanScore = 0;
 
-    /* Set screen to mode 3 */
-    SetMode(MODE_3 | BG2_ON);
+    computerPlayer.x = SCREEN_WIDTH - PADDLE_WIDTH - 1;
+    computerPlayer.y = PLAYER_START_Y;
+    computerPlayer.prevX = computerPlayer.x;
+    computerPlayer.prevY = computerPlayer.y;
+    computerPlayer.width = PADDLE_WIDTH;
+    computerPlayer.height = PADDLE_HEIGHT;
+    computerPlayer.velocityX = 0;
+    computerPlayer.velocityY = 0;
+    computerScore = 0;
+
+    ball.x = BALL_START_X;
+    ball.y = (SCREEN_HEIGHT / 2) - (BALL_SIZE / 2);
+    ball.prevX = ball.x;
+    ball.prevY = ball.y;
+    ball.width = BALL_SIZE;
+    ball.height = BALL_SIZE;
+    ball.velocityX = 2;
+    ball.velocityY = 2;
+
+    pauseLength = NEW_GAME_PAUSE;
+    pauseCounter = 0;
 
     /* Main Game Loop */
     while (1)
@@ -321,33 +291,14 @@ int main(void)
         VBlankIntrWait();
         scanKeys();
 
-        if (gameMode == MATCH_MODE)
-        {
-            matchMode(&humanPlayer,
-                      &computerPlayer,
-                      &ball,
-                      &humanScore,
-                      &computerScore,
-                      &pauseLength,
-                      &pauseCounter,
-                      &paused,
-                      &gameMode);
+        matchMode(&humanPlayer,
+                  &computerPlayer,
+                  &ball,
+                  &humanScore,
+                  &computerScore,
+                  &pauseLength,
+                  &pauseCounter);
 
-            /* Reset after completed game */
-        }
-        else if (gameMode == RESET_MODE)
-        {
-            if (pauseCounter > pauseLength)
-            {
-                pauseCounter = 0;
-                clearScreen();
-                gameMode = MENU_MODE;
-            }
-            else
-            {
-                pauseCounter++;
-            }
-        }
-
+        /* Reset after completed game */
     }
 }
